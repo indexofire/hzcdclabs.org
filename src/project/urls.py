a9a17ffe1c43d9bea12040ba18252c6ba9b3da42
Mark Renton <indexofire@gmail.com>
2012-06-17 21:55:24 +0800

    soo

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,14 +2,26 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from feincms.module.page.sitemap import PageSitemap
 
 admin.autodiscover()
+
+# create sitemap
+sitemaps = {
+    'page': PageSitemap,
+}
 
 urlpatterns = patterns('',
     # url(r'^$', 'cdclabs.views.home', name='home'),
     # url(r'^cdclabs/', include('cdclabs.foo.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', 
+        {'sitemaps': sitemaps}
+    ),
 )
 
 if settings.DEBUG:
@@ -19,6 +31,7 @@ if settings.DEBUG:
         ),
     )
 
+# 
 urlpatterns += patterns('',
     url(r'', include('feincms.urls')),
 )
